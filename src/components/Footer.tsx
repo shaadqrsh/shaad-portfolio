37eb3644aa3a6d7228c37693b55ebf42b4670492
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-05-03 23:14:17 +0530

    nav and fot

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Github, LucideLinkedin, Mail, Twitter } from "lucide-react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
-    <div>Footer</div>
-  )
-}
+    <section className="p-4 bg-shaad-200 grid grid-cols-3 justify-evenly items-center">
+      <div className="flex justify-center items-center gap-x-4">
+        <p className="text-white font-bold text-md">Get in Touch</p>
+        <LucideLinkedin className="text-white" />
+        <Twitter className="text-white" />
+        <Github className="text-white" />
+        <Mail className="text-white" />
+      </div>
+      <p className="text-white text-md mx-auto flex">
+        Copyright ©️ {currentYear - 1} - {currentYear} | All rights reserved
+      </p>
+      <p className="text-white text-md mx-auto flex">
+        Designed By{" "}
+        <a
+          href="https://hardikportfolio-hardik-malhotra.vercel.app"
+          target="_blank"
+          className="underline hover:text-blue-400 ml-1"
+        >
+          Hardik Malhotra
+        </a>
+      </p>
+    </section>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,23 @@
 import { headers } from "@/lib/header";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 z-50 backdrop-blur-sm bg-white/10 border-b border-shaad-200/20 shadow-md">
-      <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-center">
-        {/* <div className="text-xl font-bold text-gray-800">Brand</div> */}
-        <ul className="flex space-x-6">
+    <nav className="sticky top-0 z-50 backdrop-blur-md bg-shaad-200 shadow-md">
+      <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
+        <div className="flex justify-between items-center">
+          <h1 className="text-lg text-white font-bold tracking-wider">
+            Shaad Qureshi
+          </h1>
+        </div>
+        <ul className="flex space-x-6 text-gray-700 font-medium ml-auto">
           {headers.map((h) => {
             return (
               <li key={h.label}>
                 <Link
                   href={h.link}
-                  className="text-white tracking-wider font-semibold"
+                  className="text-white tracking-wider font-semibold text-lg hover:text-shaad-400 transition"
                 >
                   {h.label}
                 </Link>
@@ -20,6 +25,12 @@ const Navbar = () => {
             );
           })}
         </ul>
+        <Button
+          className="cursor-pointer bg-transparent text-lg ml-4 hover:bg-shaad-400 hover:text-white"
+          variant="link"
+        >
+          Download CV
+        </Button>
       </div>
     </nav>
   );
