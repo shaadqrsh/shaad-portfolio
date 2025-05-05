013a98be66866e19764dba7abf088dade389cbb4
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-05-05 19:14:05 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,27 +22,31 @@ const links = [
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <section className="p-4 bg-shaad-200 grid grid-cols-3 justify-evenly items-center">
-      <div className="flex justify-center items-center gap-x-4">
-        <p className="text-white font-bold text-sm">Get in Touch</p>
-        {links.map((l, idx) => {
-          return (
-            <a
-              href={l.href}
-              key={idx}
-            >
-              <l.label
-                className="text-white"
-                size={18}
-              />
-            </a>
-          );
-        })}
+    <section className="p-4 bg-shaad-200 grid grid-cols-3 justify-evenly items-center max-sm:flex max-sm:flex-col max-sm:gap-y-4">
+      <div className="flex flex-col justify-center items-center gap-x-4">
+        <p className="text-white font-bold min-lg:text-sm text-xs">
+          Get in Touch
+        </p>
+        <div className="flex gap-x-4 mt-2">
+          {links.map((l, idx) => {
+            return (
+              <a
+                href={l.href}
+                key={idx}
+              >
+                <l.label
+                  className="text-white min-lg:text-sm text-xs hover:text-shaad-400 transition"
+                  size={18}
+                />
+              </a>
+            );
+          })}
+        </div>
       </div>
-      <p className="text-white text-sm mx-auto flex">
+      <p className="text-white min-lg:text-sm text-xs mx-auto flex text-center">
         Copyright ©️ {currentYear - 1} - {currentYear} | All rights reserved
       </p>
-      <p className="text-white text-sm mx-auto flex">
+      <p className="text-white min-lg:text-sm text-xs mx-auto flex">
         Designed By{" "}
         <a
           href="https://hardikportfolio-hardik-malhotra.vercel.app"
