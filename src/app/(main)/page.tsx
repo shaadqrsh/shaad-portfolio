4ae15aed36e25d8133a6a43f09f64f95dae610a2
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-05-06 17:06:27 +0530

    text animation

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,40 +1,67 @@
+"use client";
+import Reveal from "@/components/Reveal";
 import { homePara } from "@/lib/Data";
 import { links } from "@/lib/header";
-import Image from "next/image";
+import { motion } from "motion/react";
 
 const Home = () => {
   return (
     <section className="bg-shaad-400 text-white h-full flex items-center justify-center px-6">
       <div className="max-w-6xl mx-auto flex flex-col-reverse md:flex-row items-center gap-12">
-        <div className="flex-1 border-shaad-100 border-2 p-8 rounded-4xl shadow-2xl">
-          <h2 className="text-xl mb-2 text-shaad-600">
-            Professional ChutPaglu
-          </h2>
-          <h1 className="text-4xl md:text-5xl font-bold mb-2">Shaad Qureshi</h1>
-          <p className="text-gray-400 my-5 max-w-md">{homePara}</p>
-          <div className="flex gap-5">
-            {links.map((l, idx) => {
-              return (
-                <a
-                  href={l.href}
-                  key={idx}
-                >
-                  <l.label
-                    className="text-white min-lg:text-sm text-xs hover:text-shaad-100 transition"
-                    size={25}
-                  />
-                </a>
-              );
-            })}
-          </div>
-        </div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="flex-1 border-shaad-100 border-2 p-8 rounded-4xl shadow-2xl"
+        >
+          <Reveal
+            x={-100}
+            de={0.2}
+          >
+            <h2 className="text-xl mb-2 text-shaad-600">
+              Professional ChutPaglu
+            </h2>
+          </Reveal>
+          <Reveal
+            x={-100}
+            de={0.2}
+          >
+            <h1 className="text-4xl md:text-5xl font-bold mb-2">
+              Shaad Qureshi
+            </h1>
+          </Reveal>
+          <Reveal
+            x={-100}
+            de={0.2}
+          >
+            <p className="text-gray-400 my-5 max-w-md">{homePara}</p>
+          </Reveal>
+          <Reveal
+            y={100}
+            de={0.2}
+          >
+            <div className="flex gap-5">
+              {links.map((l, idx) => {
+                return (
+                  <a
+                    href={l.href}
+                    key={idx}
+                  >
+                    <l.label
+                      className="text-white min-lg:text-sm text-xs hover:text-shaad-100 transition"
+                      size={25}
+                    />
+                  </a>
+                );
+              })}
+            </div>
+          </Reveal>
+        </motion.div>
 
         <div className="relative flex-1 ml-4 max-sm:mx-auto">
-          <Image
-            src="vercel.svg"
-            width={100}
-            height={100}
-            alt="Alina Smith"
+          <motion.img
+            src="/shaad1.png"
+            alt="Shaad Qureshi"
             className="relative z-10 w-full max-w-sm object-contain"
           />
         </div>
