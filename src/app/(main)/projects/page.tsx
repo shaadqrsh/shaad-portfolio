f91b98ce428d3b1ee949771bb4c45c23ebbe0f5f
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-05-16 23:06:36 +0530

    Update page.tsx

--- src/app/(main)/projects/page.tsx
+++ src/app/(main)/projects/page.tsx
@@ -1,5 +1,23 @@
+"use client";
+import ProjectsCard from "@/components/ProjectsCard";
+import projects from "@/lib/Projects";
+import { motion } from "motion/react";
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="text-white h-full w-full flex flex-col justify-center p-6 max-md:pb-16">
+      <motion.div className="grid grid-cols-2 justify-center items-center gap-6">
+        {projects.map((p, idx) => (
+          <ProjectsCard
+            title={p.title}
+            desp={p.desp}
+            img={p.img}
+            key={idx}
+          />
+        ))}
+      </motion.div>
+    </section>
+  );
 };
 
 export default Projects;
