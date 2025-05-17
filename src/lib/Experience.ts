fd9a80db31a57daab651de01738c0ae441e75fec
Shaad Qureshi <56550424+shaadqrsh@users.noreply.github.com>
2025-05-17 18:03:06 +0530

    Added experience

--- src/lib/Experience.ts
+++ src/lib/Experience.ts
@@ -1,17 +1,10 @@
 export const experience = [
   {
-    title: "UI/UX Designer",
-    year: "2013 - 2022",
-    subtitle: ["sex", "maut"],
-  },
-  {
-    title: "UI/UX Designer",
-    year: "2013 - 2022",
-    subtitle: ["sex", "maut"],
-  },
-  {
-    title: "UI/UX Designer",
-    year: "2013 - 2022",
-    subtitle: ["sex", "maut"],
-  },
+    title: "Freelance Game Developer",
+    year: "Jan 2023 - Apr 2025",
+    subtitle: [
+      "Responsibility of developing game mechanics in Unity (C#) and Unreal Engine (C++ and Blueprints).",
+       "Worked on 62 different projects for 36 different clients from different private Discord servers."
+      ],
+  }
 ];
