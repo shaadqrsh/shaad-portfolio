323159719f48ce3a981b8638d76a832e7adcabd5
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-05-17 12:19:30 +0530

    Update Skills.ts

--- src/lib/Skills.ts
+++ src/lib/Skills.ts
@@ -1,6 +1,6 @@
 export const top3 = [
   {
-    icon: "/unity.png",
+    icon: "/skills/icon_unity.svg",
     title: "Unity",
     subTitle: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, sit at
             vel nesciunt enim, cumque facere eligendi ut voluptate odit maiores
@@ -8,7 +8,7 @@ export const top3 = [
             quibusdam.`,
   },
   {
-    icon: "/unreal4.png",
+    icon: "/skills/icon_unreal.svg",
     title: "Unreal Engine",
     subTitle: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, sit at
             vel nesciunt enim, cumque facere eligendi ut voluptate odit maiores
@@ -16,11 +16,74 @@ export const top3 = [
             quibusdam.`,
   },
   {
-    icon: "/blender2.png",
+    icon: "/skills/icon_blender.svg",
     title: "Blender",
     subTitle: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, sit at
             vel nesciunt enim, cumque facere eligendi ut voluptate odit maiores
             explicabo cupiditate modi, molestiae rerum fuga non. Recusandae,
             quibusdam.`,
+  },
+];
+
+export const skills = [
+  {
+    icon: "/skills/icon_android.svg",
+    name: "Android",
+  },
+  {
+    icon: "/skills/icon_aseprite.svg",
+    name: "Aseprite",
+  },
+  {
+    icon: "/skills/icon_cs.svg",
+    name: "C#",
+  },
+  {
+    icon: "/skills/icon_cpp.svg",
+    name: "CPP",
+  },
+  {
+    icon: "/skills/icon_git.svg",
+    name: "Git",
+  },
+  {
+    icon: "/skills/icon_godot.svg",
+    name: "Godot",
+  },
+  {
+    icon: "/skills/icon_java.svg",
+    name: "Java",
+  },
+  {
+    icon: "/skills/icon_microsoft.svg",
+    name: "Microsoft",
+  },
+  {
+    icon: "/skills/icon_photoshop.svg",
+    name: "Photoshop",
+  },
+  {
+    icon: "/skills/icon_premiere.svg",
+    name: "Premiere",
+  },
+  {
+    icon: "/skills/icon_r.svg",
+    name: "R",
+  },
+  {
+    icon: "/skills/icon_ruby.svg",
+    name: "Ruby",
+  },
+];
+
+export const nameless = [
+  {
+    title: "Shaad",
+  },
+  {
+    title: "Shaad",
+  },
+  {
+    title: "Shaad",
   },
 ];
