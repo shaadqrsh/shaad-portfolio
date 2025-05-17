34459a8c8044d22b86509f9a59c7d25b217e6ec3
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-05-17 12:19:48 +0530

    u

--- src/components/sections/Experience.tsx
+++ src/components/sections/Experience.tsx
@@ -11,7 +11,7 @@ const Experience = () => {
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.3 * idx }}
+            transition={{ duration: 0.5 }}
             viewport={{ once: true }}
             key={idx}
           >
