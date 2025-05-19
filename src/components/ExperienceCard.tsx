4f90f34305cc4dd19e0797540c12e35488cd47ad
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-05-19 13:46:29 +0530

    Update ExperienceCard.tsx

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -23,23 +23,34 @@ const ExperienceCard = ({
       {ex && (
         <div className="w-3 h-3 bg-shaad-600 rounded-full z-10 relative -left-[6.5px] self-center" />
       )}
-      <div className="bg-shaad-300 p-4 rounded-xl shadow-md hover:shadow-xl transition-shadow duration-200 w-full">
-        <h2 className="text-lg font-semibold text-white">{title}</h2>
-        {ex && <p className="text-md text-shaad-500 mb-2">{date}</p>}
+      <div className="bg-shaad-300 p-4 rounded-xl shadow-md hover:shadow-xl transition-shadow duration-200 w-full flex">
+        {/* {ex && (
+          <motion.img
+            src={"/img_home.png"}
+            width={100}
+            height={100}
+            className="mr-2"
+          />
+        )} */}
 
-        {subTitle &&
-          (Array.isArray(subTitle) ? (
-            subTitle.map((sub) => (
-              <p
-                className="text-sm text-white"
-                key={sub}
-              >
-                • {sub}
-              </p>
-            ))
-          ) : (
-            <p className="text-sm text-white">{subTitle}</p>
-          ))}
+        <div>
+          <h2 className="text-lg font-semibold text-white">{title}</h2>
+          {ex && <p className="text-md text-shaad-500 mb-2">{date}</p>}
+
+          {subTitle &&
+            (Array.isArray(subTitle) ? (
+              subTitle.map((sub) => (
+                <p
+                  className="text-sm text-white mt-1"
+                  key={sub}
+                >
+                  • {sub}
+                </p>
+              ))
+            ) : (
+              <p className="text-sm text-white">{subTitle}</p>
+            ))}
+        </div>
       </div>
     </motion.div>
   );
