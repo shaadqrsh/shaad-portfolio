6305dad9be71a8828c59cba2eb8034aa12d16c25
Shaad Qureshi <56550424+shaadqrsh@users.noreply.github.com>
2025-05-22 13:41:18 +0530

    Added Project details

---
+++ src/lib/Project_bushido.ts
@@ -0,0 +1,35 @@
+export const data = {
+    title: "Pokémon Bushido",
+    year: "2021",
+    download: "https://drive.google.com/file/d/1IZ3AlFv5rm8xrUn9YbwLe4i-Bu9YwHpO/view",
+    desc: "peepee poopoo",
+    features: [
+        {
+            heading: "Feature 1",
+            content: "penus"
+        },
+        {
+            heading: "Feature 2",
+            content: "balls"
+        }
+    ],
+    technologies: [
+        "Your mom",
+        "19 inches of venom"
+    ],
+    responsibilities: [
+        "Gave head to your mom",
+        "conducted 9/11",
+        "Ate kusum roll"
+    ],
+    videos: [
+        "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        "https://www.youtube.com/watch?v=4nwAra0mz_Q",
+        "https://www.youtube.com/watch?v=zWSdit919E0"
+
+    ],
+    additional: [
+        "This game was developed in to impress your mom",
+        "She gave me insane head"
+    ]
+};
