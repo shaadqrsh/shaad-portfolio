2806edc2edfc950d6f0f5b09065d5fb164b3cf56
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-05-22 14:07:21 +0530

    setup

--- src/app/(main)/projects/[project]/page.tsx
+++ src/app/(main)/projects/[project]/page.tsx
@@ -1,12 +1,49 @@
 "use client";
-import { useParams } from "next/navigation";
+import { data } from "@/lib/projectId";
+import { projectData } from "@/types";
+import { Loader2 } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const ProjectIdPage = () => {
-  const { project: title } = useParams();
+  const [project, setProject] = useState<projectData | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const params = useParams();
+  const router = useRouter();
+
+  const title = Array.isArray(params.project)
+    ? params.project[0]
+    : params.project;
+
+  useEffect(() => {
+    setLoading(true);
+    if (title && typeof title === "string" && title in data) {
+      setProject(data[title]);
+    } else {
+      router.back();
+    }
+    setLoading(false);
+  }, [title, router]);
+
+  if (loading) {
+    return <Loader2 className="animate-spin text-white" />;
+  }
+
   return (
-    <div>
-      <h1>{title}</h1>
-    </div>
+    <section className="text-white h-full w-full flex flex-col justify-center items-center p-6 max-md:pb-16">
+      <div className="grid grid-cols-2 justify-between items-center gap-4">
+        <div className="p-2 border border-shaad-100 rounded-lg flex flex-col items-start">
+          <h1 className="text-shaad-600">Description</h1>
+          <p>{project?.desc}</p>
+        </div>
+
+        <div className="p-2 border border-shaad-100 rounded-lg flex flex-col items-start">
+          <h1 className="text-shaad-600 font-bold">Technology Used</h1>
+          <p>desp</p>
+        </div>
+      </div>
+    </section>
   );
 };
 
---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type projectData = {
+  title: string;
+  year: string;
+  game: boolean;
+  download: string;
+  desc: string;
+  features: { heading: string; content: string }[];
+  technologies: string[];
+  responsibilities: string[];
+  videos: string[];
+  additional: string[];
+};
