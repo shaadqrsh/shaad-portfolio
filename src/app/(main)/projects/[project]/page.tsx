884c71803dc5e733343d4c2ca52d77054e2b2fea
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-05-23 11:12:41 +0530

    hm

--- src/app/(main)/projects/[project]/page.tsx
+++ src/app/(main)/projects/[project]/page.tsx
@@ -1,7 +1,9 @@
 "use client";
+import Title from "@/components/Title";
 import { data } from "@/lib/projectId";
 import { projectData } from "@/types";
 import { Loader2 } from "lucide-react";
+import { motion } from "motion/react";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -12,47 +14,71 @@ const ProjectIdPage = () => {
   const params = useParams();
   const router = useRouter();
 
-  const title = Array.isArray(params.project)
+  const url = Array.isArray(params.project)
     ? params.project[0]
     : params.project;
 
   useEffect(() => {
     setLoading(true);
-    if (title && typeof title === "string" && title in data) {
-      setProject(data[title]);
+    if (url && typeof url === "string" && url in data) {
+      setProject(data[url]);
     } else {
       router.back();
     }
     setLoading(false);
-  }, [title, router]);
+  }, [url, router]);
 
   if (loading) {
     return <Loader2 className="animate-spin text-white" />;
   }
 
   return (
-    <section className="text-white h-full w-full flex flex-col justify-center items-center p-6 max-md:pb-16">
-      <div className="grid grid-cols-2 justify-between items-center gap-4">
-        <div className="p-2 border border-shaad-100 rounded-lg flex flex-col items-start">
-          <h1 className="text-shaad-600">Description</h1>
-          <p>{project?.desc}</p>
-          <h1 className="text-shaad-600 mt-4">Features</h1>
-          {project?.features.map((f, idx) => (
-            <div
-              className="flex flex-col items-start"
-              key={idx.toString()}
-            >
-              <h2>-{f.heading}</h2>
-              <h3>{f.content}</h3>
-            </div>
-          ))}
-        </div>
+    <section className="text-white h-full w-full flex flex-col justify-center items-center max-md:pb-16">
+      {/* <motion.img
+        src={`/project_${url}/cover.jpg`}
+        className="pb-4"
+      /> */}
 
-        <div className="p-2 border border-shaad-100 rounded-lg flex flex-col items-start">
-          <h1 className="text-shaad-600 font-bold">Technology Used</h1>
-          <p>desp</p>
-        </div>
-      </div>
+      <section className="text-white h-full w-full flex flex-col justify-center p-6 items-center">
+        <motion.div className="grid grid-cols-2 justify-between items-start gap-4">
+          <div className="p-4 border border-shaad-100 rounded-lg flex flex-col items-start">
+            <Title classN="mb-0">Des</Title>
+            <p className=" lg:text-base text-sm">{project?.desc}</p>
+
+            <Title classN="mb-0 mt-4">Features</Title>
+            {project?.features.map((f, idx) => (
+              <div
+                className="flex flex-col items-start"
+                key={idx.toString()}
+              >
+                <h2 className="font-semibold mt-2">-{f.heading}</h2>
+                <h3>{f.content}</h3>
+              </div>
+            ))}
+          </div>
+
+          <div className="p-4 border border-shaad-100 rounded-lg flex flex-col items-start">
+            <Title classN="mb-0">Technology Used</Title>
+            <div className="flex flex-wrap items-center gap-4">
+              {project?.technologies.map((t, idx) => (
+                <div
+                  key={idx.toString()}
+                  className="p-2 bg-shaad-600 mt-2 rounded-lg"
+                >
+                  <p>{t}</p>
+                </div>
+              ))}
+            </div>
+
+            <Title classN="mt-6 mb-2">Key Responsibilities</Title>
+            {project?.responsibilities.map((r, idx) => (
+              <div key={idx.toString()}>
+                <p className="lg:text-base text-sm font-semibold">-{r} </p>
+              </div>
+            ))}
+          </div>
+        </motion.div>
+      </section>
     </section>
   );
 };
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,18 +1,22 @@
 "use client";
+import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 import Reveal from "./Reveal";
 
 interface TitleProps {
   children: ReactNode;
+  classN?: string;
 }
 
-const Title = ({ children }: TitleProps) => {
+const Title = ({ children, classN }: TitleProps) => {
   return (
     <Reveal
       x={-100}
       de={0.2}
     >
-      <h2 className="text-4xl mb-4 font-bold text-shaad-600">{children}</h2>
+      <h2 className={cn("text-4xl mb-4 font-bold text-shaad-600", classN)}>
+        {children}
+      </h2>
     </Reveal>
   );
 };
