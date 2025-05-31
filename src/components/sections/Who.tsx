3299bdf177b80912943c3aab1e6fcaefdad62d76
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-05-31 17:34:26 +0530

    whoam i

--- src/components/sections/Who.tsx
+++ src/components/sections/Who.tsx
@@ -1,5 +1,6 @@
-import { whoAmI } from "@/lib/Data";
+import { idk, whoAmI } from "@/lib/Data";
 
+import { motion } from "motion/react";
 import AboutCard from "../AboutCard";
 import Reveal from "../Reveal";
 
@@ -12,6 +13,33 @@ const Who = () => {
       >
         <p className="font-light tracking-wide">{whoAmI}</p>
       </Reveal>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        viewport={{ once: true }}
+        className="flex justify-around items-center flex-wrap mt-4 gap-4"
+      >
+        {idk.map((i, idx) => (
+          <div
+            key={idx}
+            className="p-4 rounded-xl border-2 border-shaad-600 flex flex-col justify-around items-center"
+          >
+            <Reveal
+              x={-100}
+              de={0.2}
+            >
+              <h1 className="md:text-2xl">{i.count}</h1>
+            </Reveal>
+            <Reveal
+              x={-100}
+              de={0.2}
+            >
+              <h1 className="md:text-2xl">{i.label}</h1>
+            </Reveal>
+          </div>
+        ))}
+      </motion.div>
     </AboutCard>
   );
 };
--- src/lib/Data.ts
+++ src/lib/Data.ts
@@ -1,3 +1,18 @@
 export const homePara = `I'm a creative thinker with a computer science background, passionate about using game development to tell stories and build worlds. Skilled in Unity and Unreal, I aspire to develop innovative games that captivate players and eventually establish my own unique studio.`;
 
 export const whoAmI = `For me, game development is the ultimate fusion of art and technology – a medium to tell compelling stories and build entire worlds for others to explore. This belief, born from a childhood fascination with games and nurtured by my computer science education, drives my ambition. I've proactively honed my skills in engines like Unity and Unreal, delved into asset creation with Photoshop and Blender, and gained practical experience through freelance projects. My aspiration is not just to develop games, but to craft meaningful experiences that resonate with players, eventually leading to the creation of my own studio where I can foster a new generation of creators.`;
+
+export const idk = [
+  {
+    label: "Experince",
+    count: "1+",
+  },
+  {
+    label: "Projects",
+    count: "10+",
+  },
+  {
+    label: "Clients",
+    count: "-1",
+  },
+];
