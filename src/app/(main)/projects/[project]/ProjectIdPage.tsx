65d797eef20ca62d6e4c6a5d6c8e934992422c1e
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-06-04 15:49:51 +0530

    coverimg

--- src/app/(main)/projects/[project]/ProjectIdPage.tsx
+++ src/app/(main)/projects/[project]/ProjectIdPage.tsx
@@ -5,11 +5,11 @@ import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-import { ChevronDown } from "lucide-react";
 import { motion } from "motion/react";
 
 import { Parallax } from "react-scroll-parallax";
 
+import CoverImg from "@/components/projects/CoverImg";
 import Title from "@/components/Title";
 import {
   Carousel,
@@ -18,12 +18,6 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
 
 export default function ProjectIdPage({ url }: { url: string }) {
@@ -54,41 +48,12 @@ export default function ProjectIdPage({ url }: { url: string }) {
         speed={-50}
         className={`h-[75vh] w-full mt-[-10vh]`}
       >
-        <div className="relative w-full h-full flex items-center justify-center overflow-hidden">
-          <motion.img
-            src={`/project_${url}/cover.jpg`}
-            alt={`${project?.title || url} cover`}
-            className="absolute inset-0 w-full h-full object-cover object-center blur-xs"
-          />
-
-          <div className="absolute inset-0 bg-black/40" />
-
-          <div className="relative z-10 p-8 border-[3px] border-shaad-600 rounded-xl bg-black/60 flex flex-col justify-center items-center">
-            <h2 className="text-4xl text-white font-bold">{project.title}</h2>
-
-            <DropdownMenu>
-              <DropdownMenuTrigger className="flex gap-x-2 justify-center items-center p-2 rounded-lg bg-shaad-600 text-white mt-8">
-                Download
-                <ChevronDown size={20} />
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="bg-shaad-600 border-shaad-500 text-white">
-                {project.urls.map((u, idx) => (
-                  <a
-                    key={idx}
-                    href={u.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <DropdownMenuItem className="bg-shaad-600 cursor-pointer hover:bg-shaad-500 transition">
-                      {u.label}
-                    </DropdownMenuItem>
-                  </a>
-                ))}
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        </div>
+        <CoverImg
+          project={project}
+          url={url}
+        />
       </Parallax>
+
       <motion.section
         className={`text-white flex flex-col justify-center p-6 items-center rounded-t-2xl bg-shaad-400 relative z-10 mt-[5vh]`}
       >
---
+++ src/components/projects/CoverImg.tsx
@@ -0,0 +1,64 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { projectData } from "@/types";
+import { ChevronDown } from "lucide-react";
+import { motion } from "motion/react";
+import Reveal from "../Reveal";
+
+interface CoverImgProps {
+  project: projectData;
+  url: string;
+}
+
+const CoverImg = ({ project, url }: CoverImgProps) => {
+  return (
+    <div className="relative w-full h-full flex items-center justify-center overflow-hidden">
+      <motion.img
+        src={`/project_${url}/cover.jpg`}
+        alt={`${project?.title || url} cover`}
+        className="absolute inset-0 w-full h-full object-cover object-center blur-xs"
+      />
+
+      <div className="absolute inset-0 bg-black/40" />
+
+      <div className="relative z-10 p-8 border-[3px] border-shaad-600 rounded-xl bg-black/60 flex flex-col justify-center items-center">
+        <Reveal x={-150}>
+          <h2 className="text-4xl text-white font-bold">{project.title}</h2>
+        </Reveal>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger className="flex gap-x-2 justify-center items-center p-2 rounded-lg bg-shaad-600 text-white mt-8">
+            Download
+            <ChevronDown size={20} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="bg-shaad-600 border-shaad-500 text-white">
+            {project.urls.map((u, idx) => (
+              <a
+                key={idx}
+                href={u.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <DropdownMenuItem className="bg-shaad-600 cursor-pointer hover:bg-shaad-500 transition">
+                  {u.label}
+                </DropdownMenuItem>
+              </a>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <Reveal x={-150}>
+          <h2 className="text-xl mt-6 text-white font-bold">
+            Released: {project.year}
+          </h2>
+        </Reveal>
+      </div>
+    </div>
+  );
+};
+
+export default CoverImg;
