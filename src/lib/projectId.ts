377b8a552bea57b3679893bf6b53230008d1e5df
Shaad Qureshi <56550424+shaadqrsh@users.noreply.github.com>
2025-06-10 23:49:36 +0530

    Added Findly

--- src/lib/projectId.ts
+++ src/lib/projectId.ts
@@ -218,69 +218,61 @@ export const data: { [key: string]: projectData } = {
     ],
   },
   findly: {
-    title: "Findly - Lost and Found Application",
-    year: "2025",
+    title: 'Findly - Lost and Found Application',
+    year: '2025',
     game: false,
-    imgCount: 0,
+    imgCount: 9,
     urls: [
       {
-        label: "Direct Download",
-        icon: "",
-        url: "https://drive.google.com/file/d/1IZ3AlFv5rm8xrUn9YbwLe4i-Bu9YwHpO/view",
-      },
-      {
-        label: "Eevee Expo",
-        icon: "",
-        url: "https://eeveeexpo.com/splice/",
-      },
+        label: 'GitHub',
+        icon: '',
+        url: 'https://github.com/shaadqrsh/findly-android-studio/',
+      }
     ],
-    desc: "Assume the role of a research assistant aiding Professor Cypress in the study of newly discovered Pokémon forms in this new fangame. Embark on a journey through the Stacona region with a new starter Pokémon, Arenay, which possesses the ability to change its type. Align with either Team Biogress, emphasizing natural development, or Team Quantech, focused on technological enhancement. Experience a branching storyline, where you are presented with significant choices which will completely alter your experience. The fate of Pokémon research hangs in the balance - whose side will you choose?",
+    desc: 'A comprehensive Android application designed to help users connect and exchange information about lost and found items. It provides a platform where users can post detailed listings of items they have lost or found, complete with categories, descriptions, and images. The app facilitates direct communication between users through an integrated real-time chat system, allowing for efficient resolution and return of items. It also includes robust user management features, personal item listings, and a dynamic interface for a seamless user experience.',
     features: [
       {
-        heading: "New Region",
+        heading: 'Instant Item Updates',
         content:
-          "Pokémon Splice is set in the entirely new Stacona Region, a brand new world with its own unique environments and never before seen Pokémon species.",
+          'The live, continuously updating feed of lost and found items lets users see new item posts and existing item updates instantly, ensuring they always have the most current information.',
       },
       {
-        heading: "New Pokémon Species",
+        heading: 'In-App Chat Feature',
         content:
-          "The game features several new species of Pokémon, such as Arenay, the starter with a unique form changing gimmick, as well as evolutions and pre-evolutions for existing Pokémon like Pinsir, Skarmory and Combee and more!",
+          'Users can participate in private conversations directly within the app, facilitating direct and secure communication between 2 people to discuss item details and coordinate returns.',
       },
       {
-        heading: "Branching Storylines",
+        heading: 'Camera and Gallery Integration',
         content:
-          "Players make a pivotal choice to join one of two opposing research organizations: Team Biogress or Team Quantech. This decision significantly influences the story's direction and the player's objectives throughout the game.",
+          "The app has robust photo upload capabilities, enabling users to attach images to item listings and profile pictures directly from their device's camera or photo gallery.",
       },
       {
-        heading: "New Regional Forms",
+        heading: 'Secure Contact Sharing',
         content:
-          "The region houses brand new regional forms. Some of them are naturally occuring, like Dunsparce, and some are the result of experimentation by the 2 research factions, such as the new forms of the Solosis, Lotad, Raichu and Electivire.",
+          'Chats feature an integrated option for users to securely share their personal contact details (phone number and/or email), facilitating direct contact while maintaining privacy.',
       },
       {
-        heading: "Distinct Gen-3 Artsyle",
+        heading: 'Personalized Item Browsing Views',
         content:
-          "The game pays hommage to the GameBoy Advance generation of Pokémon games with its environment and UI design, but still has all the QOL improvements of the modern games.",
+          'The app offers distinct browsing categories, each with dynamic search capability: "Lost" and "Found" for general listings, and a "Mine" section for the current user\'s items.',
       },
     ],
-    technologies: ["RGSS", "Ruby", "C++", "Photoshop", "Aseprite"],
+    technologies: ['Java', 'XML', 'Android Studio', 'Firebase', 'Glide'],
     responsibilities: [
-      "Developed the core engine in which the game was made.",
-      "Created new mechanics (Form changing, mid-battle dialogue, Following Pokémon, battle animations, etc) to differenciate it from mainline Pokémom games.",
-      "Aggregated publically available assets for developing the game within 30 days.",
-      "Evented multiple in-game cutscenes, while also writing custom scripts for functionality.",
-      "Designed various UI/UX elements to enhance the immersion of the battle system, with detailed battle backgrounds and themes to fit the Generation 3 aesthetic.",
-      "Assisted in the design of the game's narrative and core gameplay loop.",
+      'Designed, implemented, and maintained the client-side Android application',
+      'Structured the Firebase Realtime Database for storage and retrieval of user profiles, item listings, and real-time messages.',
+      'Implemented custom user registration and login flows, managing user sessions, and ensuring that users can only access and modify data relevant to them.',
+      'Developed image capture/select functionality, and integrating with Firebase Storage for secure image uploads and retrieval.',
+      'Developing layouts, implementing responsive UI elements, smooth navigation, and providing clear user feedback through XML and Java.',
     ],
-    videos: [
-      "https://www.youtube.com/watch?v=B9DS0Nyegag",
-      "https://www.youtube.com/watch?v=X34OZt5Tvdk",
-      "https://www.youtube.com/watch?v=-XgNEFjSQmc&list=PLsWAsarmQkCdE1hRoOdWehJlpGgInKzNL",
-    ],
+    videos: [],
     additional: [
-      "This game was developed for the Relic Castle (Eevee Expo) Game Jam 6. It was developed in 30 days, and the team ended up winning the competition.",
-      "Future chapters were planned, and assets were developed for them (new legendaries and forms, they are still in the game files). The team, however, disbanded and further expansions were scrapped.",
-      "The development of this project was headed by Thundaga, a YouTuber, and the development of this game has been recorded as VODs on their YouTube and Twitch.",
-      "This game was discontinued in 2023 and the entire team moved on to other projects.",
+      'This project was developed as a project showcase for the subject "Advanced Mobile Programming". It was developed in a span of 3 weeks, and completed 3 months before our evaluation.',
+      'The inspiration for Findly sprang from first-hand experience of the inefficiency of poorly managed lost-and-found system on campus. The vision was to replace messy lost-and-found boxes with a streamlined digital solution.',
+      'The practical utility of the app impressed our professor and there were discussions about its potential deployment as the official lost-and-found system for the entire college campus.',
+      'Ideas like Google Maps for item location tracking, leveraging AI for image matching between lost and found items, incorporating push notifications for category alerts were considered during planning, but were scrapped for the initial release to focus on getting the key features in place first.',
+      "As a solo developer, I opted for Firebase as the backend, for its real-time features for dynamic updates and live chat. Despite the credit card requirement, this choice simplified infrastructure setup, freeing me to concentrate on the Android app's UI/UX and core logic, ultimately making it the sole project in the batch with a robust server backend.",
+      'The project utilizes a custom authentication system developed from the ground-up using Firebase and traditional encryption techniques, because I was unable to integrate Firebase Authentication. This was removed in the GitHub release, where passwords are plaintext instead.'
     ],
   },
 };
