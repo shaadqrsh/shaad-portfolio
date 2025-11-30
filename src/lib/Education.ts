65b58f6a236ec878fe8e3badba2ee87200390caa
Shaad Qureshi <56550424+shaadqrsh@users.noreply.github.com>
2025-11-30 21:23:25 +0530

    Added NEU

--- src/lib/Education.ts
+++ src/lib/Education.ts
@@ -1,21 +1,28 @@
 export const education = [
   {
-    title: "Bacherlor of Science - Information Technology",
+    title: "Masters of Science - Computer Science",
+    year: "January 2026 - May 2028 (Expected)",
+    image: "about/img_neu.jpeg",
+    subtitle:
+      "Khoury College of Computer Sciences, Northeastern University, Boston, Massachusetts, USA",
+  },
+  {
+    title: "Bachelor of Science - Information Technology",
     year: "August 2022 - June 2025",
     image: "about/img_nm.jpeg",
     subtitle:
-      "SVKM's Narsee Monjee College of Commerce and Economics, Vile Parle, Mumbai, Maharashtra",
+      "SVKM's Narsee Monjee College of Commerce and Economics, Vile Parle, Mumbai, Maharashtra, India",
   },
   {
     title: "Class 12th - HSC Board",
     year: "June 2019 - April 2021",
     image: "about/img_pace.jpeg",
-    subtitle: "PACE Junior Science College, Dadar, Mumbai, Maharashtra",
+    subtitle: "PACE Junior Science College, Dadar, Mumbai, Maharashtra, India",
   },
   {
     title: "Class 10th - ICSE Board",
     year: "June 2006 - April 2019",
     image: "about/img_bss.jpg",
-    subtitle: "Bombay Scottish School, Mahim, Mumbai, Maharashtra",
+    subtitle: "Bombay Scottish School, Mahim, Mumbai, Maharashtra, India",
   },
 ];
