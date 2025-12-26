3ca6272c26233475fc429ff48e9146baacf2e978
Shaad Qureshi <56550424+shaadqrsh@users.noreply.github.com>
2025-12-26 18:35:17 +0530

    Update Experience.ts

--- src/lib/Experience.ts
+++ src/lib/Experience.ts
@@ -2,12 +2,12 @@ export const experience = [
   {
     title: "Freelance Game Developer",
     startDate: "00/06/2023",
-    endDate: "present",
+    endDate: "00/11/2025",
     subtitle: [
       "Responsibility of developing game mechanics in Unity (C#) and Unreal Engine (C++ and Blueprints).",
       "Worked on 90+ different projects for 45+ different clients from different private Discord servers.",
     ],
     image: "about/img_freelance.png",
-    inProgress: true,
+    inProgress: false,
   },
 ];
